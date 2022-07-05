bb27d9299cb619009782fa1edf33026bc6627bdd
eugbyte <eugenetham1994@gmail.com>
2022-07-05 14:26:54 +0800

    fix: middleware

--- services/greet/main.go
+++ services/greet/main.go
@@ -14,7 +14,7 @@ func main() {
 	var address string = config.Config.LOCAL_PORT
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/hello", middlewares.Middy(hello.Handler, middlewares.LogMiddleware))
+	mux.HandleFunc("/api/hello", middlewares.Middy(hello.Handler, middlewares.LogMiddleware{}))
 
 	log.Printf("About to listen on %s. Go to https://127.0.0.1:%s/", address, address)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("127.0.0.1:%s", address), mux))
